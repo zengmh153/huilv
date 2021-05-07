cbcb7024c1ef63411cb641da81367d2f768f376f
zeng <2363187203@qq.com>
2021-05-07 13:42:12 +0800

    1

--- app/src/main/java/com/text/huilv/ConfigActivity.java
+++ app/src/main/java/com/text/huilv/ConfigActivity.java
@@ -1,3 +1,4 @@
+
 package com.text.huilv;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -5,9 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class ConfigActivity extends AppCompatActivity {
     private static final String TAG = "ConfigActivity";
+    EditText newDollar;
+    EditText newEuro;
+    EditText newWon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +24,41 @@ public class ConfigActivity extends AppCompatActivity {
         float dollar2=intent.getFloatExtra("dollar_key",0.0f);
         float euro2=intent.getFloatExtra("euro_key",0.0f);
         float won2=intent.getFloatExtra("won_key",0.0f);
-
-
-        Log.i(TAG, "onCreate_: dollar2="+dollar2);
+        Log.i(TAG, "onCreate: dollar2="+dollar2);
         Log.i(TAG, "onCreate: euro2="+euro2);
-        Log.i(TAG, "onCreateq: won2="+won2);
+        Log.i(TAG, "onCreate: won2="+won2);
+        newDollar=(EditText)findViewById(R.id.newDollar) ;
+        newEuro=(EditText)findViewById(R.id.newEuro) ;
+        newWon=(EditText)findViewById(R.id.newWon) ;
+
+        newDollar.setText(String.valueOf(dollar2));
+        newEuro.setText(String.valueOf(euro2));
+        newWon.setText(String.valueOf(won2));
+
     }
+    public void save(View btn){
+        Log.i(TAG, "save:  ");
+        float newdoller=Float.parseFloat(newDollar.getText().toString());
+        float neweuro=Float.parseFloat(newEuro.getText().toString());
+        float newwon=Float.parseFloat(newWon.getText().toString());
+        Log.i(TAG, "save: newdollar"+newdoller);
+        Log.i(TAG, "save: neweuro"+neweuro);
+        Log.i(TAG, "save: newwon"+newwon);
+        //String D = newDollar.getText().toString();
+        //newDollar_ =Float.valueOf(D);
+       // String E = newEuro.getText().toString();
+        //newEuro_ =Float.valueOf(E);
+        //String W = newWon.getText().toString();
+       // newWon_ =Float.valueOf(W);
+        Intent ret=getIntent();
+        Bundle bdl=new Bundle();
+        bdl.putFloat("key_dollar",newdoller);
+        bdl.putFloat("key_euro",neweuro);
+        bdl.putFloat("key_won",newwon);
+        ret.putExtras(bdl);
+        setResult(2,ret);
+        finish();
+
+    }
+
 }
--- app/src/main/java/com/text/huilv/RateActivity.java
+++ app/src/main/java/com/text/huilv/RateActivity.java
@@ -1,22 +1,43 @@
 package com.text.huilv;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RateActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RateActivity extends AppCompatActivity implements  Runnable {
     EditText rmb;
     TextView result_;
     private static final String TAG = "RateActivity";
-    public float dollarRate=0.1503f;
-    public float euroRate=0.1266f;
-    public float wonRate=170.2708f;
+    public float dollarRate = 0.1503f;
+    public float euroRate = 0.1266f;
+    public float wonRate = 170.2708f;
+    Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,40 +45,150 @@ public class RateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rate);
         rmb = findViewById(R.id.input_rmb);
         result_ = findViewById(R.id.result_);
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        dollarRate = sharedPreferences.getFloat("dollar_rate", 0.0f);
+        euroRate = sharedPreferences.getFloat("euro_rate", 0.0f);
+        wonRate = sharedPreferences.getFloat("won_rate", 0.0f);
+
+        //k开启线程
+        Thread t = new Thread(this);
+        t.start();
+        handler = new Handler(Looper.myLooper()) {
+            @Override
+            public void handleMessage(@Nullable Message msg) {
+                if (msg.what == 6) {
+                    String str = (String) msg.obj;
+                    Log.i(TAG, "handleMessage: str=" + str);
+                    result_.setText(str);
+                    Toast.makeText(RateActivity.this,"数据已更新",Toast.LENGTH_SHORT).show();
+                }
+                super.handleMessage(msg);
+            }
+        };
     }
-    public void click(View btn){
+
+    public void click(View btn) {
         String str = rmb.getText().toString();//获得数据
         Float M;
-        M =Float.valueOf(str);
-        float r =0.1f;
-        double R1 = 0;
-        result_.setText(String.valueOf(btn.getId()));
-        Log.i(TAG, "click: btn.getid()"+btn.getId());
-        if(str.length()==0){
-            Toast.makeText(this,"请输入计算的金额",Toast.LENGTH_SHORT).show();
-        }else if(btn.getId()==R.id.btn_dollar){
-            r=dollarRate;
-        }else if(btn.getId()==R.id.btn_euro){
-            r=euroRate;
-        }else if (btn.getId()==R.id.btn_won){
-            r=wonRate;}
-            R1=r*M;
-            result_.setText(String.format("%.2f",R1));
+        if (str.length() == 0) {
+            Toast.makeText(this, "请输入计算的金额", Toast.LENGTH_SHORT).show();
+        } else {
+            M = Float.valueOf(str);
+            float r = 0.1f;
+            double R1 = 0;
+            result_.setText(String.valueOf(btn.getId()));
+            Log.i(TAG, "click: btn.getid()" + btn.getId());
+
+            if (btn.getId() == R.id.btn_dollar) {
+                r = dollarRate;
+            } else if (btn.getId() == R.id.btn_euro) {
+                r = euroRate;
+            } else if (btn.getId() == R.id.btn_won) {
+                r = wonRate;
+            }
+            R1 = r * M;
+            result_.setText(String.format("%.2f", R1));
 
 
-    }public void onclick(View btn){
-        Intent intent=new Intent();
-        intent.setClass(RateActivity.this,ConfigActivity.class);
-        startActivityForResult(intent,6);
-        Intent config= new Intent(this,ConfigActivity.class);
-        config.putExtra("dollar_key",0);
-        config.putExtra("euro_key",0);
-        config.putExtra("won_key",0);
-
-        Log.i(TAG, "onCreate: dollarRate="+dollarRate);
-        Log.i(TAG, "onCreate: euroRate="+euroRate);
-        Log.i(TAG, "onCreate: wonRate="+wonRate);
+        }
     }
 
+    public void onclick(View btn) {
+        Intent config = new Intent(this, ConfigActivity.class);
+        config.putExtra("dollar_key", dollarRate);
+        config.putExtra("euro_key", euroRate);
+        config.putExtra("won_key", wonRate);
+        Log.i(TAG, "onCreate: dollarRate=" + dollarRate);
+        Log.i(TAG, "onCreate: euroRate=" + euroRate);
+        Log.i(TAG, "onCreate: wonRate=" + wonRate);
+
+        startActivityForResult(config, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i(TAG, "onActivityResult: dollar" + requestCode);
+        Log.i(TAG, "onActivityResult: dollar1" + resultCode);
+        if (requestCode == 1 && resultCode == 2) {
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar", 0.1f);
+            euroRate = bundle.getFloat("key_euro", 0.1f);
+            wonRate = bundle.getFloat("key_won", 0.1f);
+            Log.i(TAG, "onActivityResult: doller3" + dollarRate);
+            SharedPreferences sp = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putFloat("dollar_rate", dollarRate);
+            editor.putFloat("euro_rate", euroRate);
+            editor.putFloat("won_rate", wonRate);
+            editor.apply();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    @Override
+    public void run() {
+        Log.i(TAG, "run: run.....");
+        /*try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        Log.i(TAG, "run:  after 2s");*/
+        /*URL url = null;
+        try {
+            url = new URL("http://www.usd-cny.com/bankofchina.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+            String html = inputStream2String(in);
+            Log.i(TAG, "run: html=" + html);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i(TAG, "run: ex="+e.toString());
+        }*/
+        try{
+            Document doc = Jsoup.connect("http://www.usd-cny.com/bankofchina.htm").get();
+            Log.i(TAG, "run: title="+doc.title());
+            Elements tables=doc.getElementsByTag("table");
+            Element table1=tables.first();
+            //Element table1=doc.getElementsByTag("table").first();等于上面两行
+            Elements tds=table1.getElementsByTag("td");
+            for (Element td : tds){
+                Log.i(TAG, "run: td="+td);
+                Log.i(TAG, "run: td.text"+td.text());
+                Log.i(TAG, "run: td.html"+td.html());
+            }
+           /* Elements class1=table1.getElementsByClass("bz");
+            for (Element td : class1){
+                Log.i(TAG, "run: td="+td);
+            }*/
+        }catch (IOException e){
+            e.printStackTrace();
+            Log.i(TAG, "run: "+e.toString());
+        }
+
+            Message msg = handler.obtainMessage(6, "gkd");
+            //msg.what=6;
+            //msg.obj="Hello from run()";
+            handler.sendMessage(msg);
+        }
+
+
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[ ] buffer = new char[ bufferSize];final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream,"gb2312" );
+        while (true) {
+        int rsz = in.read(buffer,0,buffer.length);
+        if(rsz<0)
+            break;
+        out.append(buffer,0,rsz);
+        }
+        return  out.toString();
+        }
 
 }
